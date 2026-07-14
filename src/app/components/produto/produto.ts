fb47401b56db9e3e4a91260bb5676b4744bb1af8
Maria Caroline de Brito Espíndola <carolbrito2026@gmail.com>
2026-07-13 21:42:34 -0300

    Alteração no projeto

--- src/app/components/produto/produto.ts
+++ src/app/components/produto/produto.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
-
+  import{ UpperCasePipe } from '@angular/common';
+  
 @Component({
   selector: 'app-produto',
-  imports: [],
+  imports: [UpperCasePipe],
   templateUrl: './produto.html',
   styleUrl: './produto.css',
 })
-export class Produto {}
+export class Produto {
+  produto = 'Notebook';
+  preco = 1500.99;
+  mostrarProduto = true;
+  mostrarPreco = true;
+  produtos = [
+    {nome:'Teclado', preco: 49.99},
+    {nome:'Mouse', preco: 29.99},
+    {nome:'Monitor', preco: 149.99}
+  ];
+}
