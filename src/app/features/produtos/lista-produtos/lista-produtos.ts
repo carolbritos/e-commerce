40ddddc4be5cfd8b896162c78d689f184c44a491
Maria Caroline de Brito Espíndola <carolbrito2026@gmail.com>
2026-07-17 18:58:58 -0300

    feat(produtos): Implementa funicionalidades de controle da lista de produtos

--- src/app/features/produtos/lista-produtos/lista-produtos.ts
+++ src/app/features/produtos/lista-produtos/lista-produtos.ts
@@ -1,20 +1,39 @@
 import { Component } from '@angular/core';
 import { Produto } from '../produto/produto';
+import { signal } from '@angular/core';
+import { computed } from '@angular/core';
+import { PrecoFormatadoPipe } from '../../../shared/pipes/preco-formatado-pipe';
+
 @Component({
   selector: 'app-lista-produtos',
-  imports: [Produto],
+  imports: [Produto, PrecoFormatadoPipe],
   templateUrl: './lista-produtos.html',
   styleUrl: './lista-produtos.css',
 })
 export class ListaProdutos {
-  produtos = [
+  //!Lista com dados - Array
+  produtos = signal( [
     {nome:'Teclado Gamer', preco:229.99},
     {nome:'Mouse Gamer', preco:129.99},
     {nome:'Monitor Gamer', preco:2000},
     {nome:'Desktop Gamer', preco:4999.99},
     {nome:'Headset Gamer', preco:500}
-  ];
+  ]);
+  //!Função para exibir produtos selecionados pelo usuario no console
   exibirProduto(nome: string){
     console.log('Produto Selecionado: ', nome);
   }
+  //!Função que adicionar produto usando metodo update()
+  adicionarProduto(){
+    this.produtos.update(listaAtual => [
+      ...listaAtual,
+      {nome:'Playstation 5', preco:3000},
+    ]);
+  }
+  //!Função que contabiliza a quantidade de produtos na lista com metodo computed()
+  totalProdutos = computed(() => this.produtos().length);
+  //!Função que calcula o valor total do produtos usando metodo computed()
+  valorTotal = computed(() =>
+  {return this.produtos().reduce((total, item) =>
+  total + item.preco,0)});
 }
