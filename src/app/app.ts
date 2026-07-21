b5bc1fdd2037231a36f2b95cfeec9d5c3461cae7
Maria Caroline de Brito Espíndola <carolbrito2026@gmail.com>
2026-07-21 20:20:10 -0300

    feat: implementa roteamento da aplicação com RouterLink e RouterOutlet

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,14 @@
 import { Component, signal } from '@angular/core';
-//import { RouterOutlet } from '@angular/router';
-//import { Produto } from './components/produto/produto';
-import { ListaProdutos } from './features/produtos/lista-produtos/lista-produtos';
+import { RouterOutlet, RouterLink} from '@angular/router';
+import { UpperCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [ListaProdutos],
+  imports: [RouterOutlet, RouterLink, UpperCasePipe],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('e-commerce');
+  nomeLoja = 'Mercado Liso';
 }
--- src/app/features/produtos/lista-produtos/lista-produtos.ts
+++ src/app/features/produtos/lista-produtos/lista-produtos.ts
@@ -64,4 +64,17 @@ export class ListaProdutos {
   }
   //! Metodo para criar um estado se seleção com signal string | null
   produtoSelecionado = signal <string | null>(null);
+  //! Metodo para criar um estado para carrinho com signal
+  carrinho = signal <{nome: string; preco: number}[]>([]);
+  adicionarAoCarrinho(produto:{nome: string; preco: number}){
+    this.carrinho.update(listaAtual => [...listaAtual,produto]
+    );
+  }
+  //!totalProdutos = computed(() => this.produtos().length);
+  //metodo para calcular a quantidade total de itens no carrinho
+  quantidadeCarrinho = computed(()=> this.carrinho().length);
+  //metodo para calcular o valor total dos itens do carrinho
+  totalCarrinho = computed(() =>{
+    return this.carrinho().reduce((total,item) =>
+      total + item.preco,0)});
 }
