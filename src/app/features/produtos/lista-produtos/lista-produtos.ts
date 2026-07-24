5435170660c027d59c2acf52a74d22d5bbcb056f
Maria Caroline de Brito Espíndola <carolbrito2026@gmail.com>
2026-07-24 18:39:15 -0300

    refactor: configurar HttpClient e refactorar lista de produtos

--- src/app/features/produtos/lista-produtos/lista-produtos.ts
+++ src/app/features/produtos/lista-produtos/lista-produtos.ts
@@ -4,52 +4,65 @@ import { signal } from '@angular/core';
 import { computed } from '@angular/core';
 import { PrecoFormatadoPipe } from '../../../shared/pipes/preco-formatado-pipe';
 import { effect } from '@angular/core';
+import { UpperCasePipe } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-lista-produtos',
-  imports: [Produto, PrecoFormatadoPipe,],
+  imports: [Produto, PrecoFormatadoPipe, UpperCasePipe],
   templateUrl: './lista-produtos.html',
   styleUrl: './lista-produtos.css',
 })
 export class ListaProdutos {
-  //!Lista com dados - Array
-  produtos = signal( [
-    {nome:'Teclado Gamer', preco:229.99},
-    {nome:'Mouse Gamer', preco:129.99},
-    {nome:'Monitor Gamer', preco:2000},
-    {nome:'Desktop Gamer', preco:4999.99},
-    {nome:'Headset Gamer', preco:500}
-  ]);
-  //!Função para exibir produtos selecionados pelo usuario no console
-  exibirProduto(nome: string){
-    console.log('Produto Selecionado: ', nome);
-    this.produtoSelecionado.set(nome);
-  }
-  //!Função que adicionar produto usando metodo update()
-  adicionarProduto(){
-    this.produtos.update(listaAtual => [
-      ...listaAtual,
-      {nome:'Playstation 5', preco:3000},
-    ]);
-  }
-  //!Função que contabiliza a quantidade de produtos na lista com metodo computed()
+  // ========== SIGNALS ================
+  produtos = signal<{nome: string; preco: number}[]>([]);
+
+  carregando = signal(true);
+
+  produtoSelecionado = signal <string | null>(null);
+
+  carrinho = signal <{nome: string; preco: number}[]>([]);
+
+  // ========== COMPUTED ================
   totalProdutos = computed(() => this.produtos().length);
-  //!Função que calcula o valor total do produtos usando metodo computed()
+
   valorTotal = computed(() =>
   {return this.produtos().reduce((total, item) =>
-  total + item.preco,0)});
-  //!Função para substutuir a lista atual usando o metodo ser()
-  substituirProdutos(){
-    this.produtos.set([
-      { nome:'Teclado', preco: 50 },
-      { nome:'Mouse', preco: 15 },
-      { nome:'Monitor', preco: 500 },
-      { nome:'Desktop', preco: 1500 },
-      { nome:'Headset', preco: 30 },
-    ]);
+  total + item.preco,0
+)});
+
+  quantidadeCarrinho = computed(()=> this.carrinho().length);
+
+  totalCarrinho = computed(() =>{
+    return this.carrinho().reduce((total,item) =>
+      total + item.preco,0
+  )});
+
+// ======== MÉTODO HTTP CLINT (API) ============
+carregarProduto(){
+ this.carregando.set(true);
+ this.http.get<{title: string; price: number}[]>
+ ('https://fakestoreapi.com/products').subscribe({
+  next: (dados) => {
+    const produtosFormatados = dados.map(p => ({
+      nome: p.title,
+      preco: p.price,
+    }));
+    this.produtos.set(produtosFormatados);
+    this.carregando.set(false);
+  },
+  error: (erro) => {
+    console.error('Erro ao carregar produtos: ', erro);
+    this.carregando.set(false);
   }
-  //! Metodo para monitorar alteralções em tempo real usando effect()
-  constructor(){
+ });
+}
+
+// ======== CONSTRUCTOR ================
+constructor(private http: HttpClient){
+    // Carrega a API
+    this.carregarProduto();
+    // Effects continuam iguais - não mexer
     effect(() =>{
       console.log('Lista de Produtos Alterados: ', this.produtos());
     });
@@ -62,19 +75,33 @@ export class ListaProdutos {
       }
     });
   }
-  //! Metodo para criar um estado se seleção com signal string | null
-  produtoSelecionado = signal <string | null>(null);
-  //! Metodo para criar um estado para carrinho com signal
-  carrinho = signal <{nome: string; preco: number}[]>([]);
-  adicionarAoCarrinho(produto:{nome: string; preco: number}){
+
+  // ======== MÉTODO UPDATE ==============
+   adicionarProduto(){
+    this.produtos.update(listaAtual => [
+      ...listaAtual,
+      {nome:'Playstation 5', preco:3000},
+    ]);
+  }
+   
+   adicionarAoCarrinho(produto:{nome: string; preco: number}){
     this.carrinho.update(listaAtual => [...listaAtual,produto]
     );
   }
-  //!totalProdutos = computed(() => this.produtos().length);
-  //metodo para calcular a quantidade total de itens no carrinho
-  quantidadeCarrinho = computed(()=> this.carrinho().length);
-  //metodo para calcular o valor total dos itens do carrinho
-  totalCarrinho = computed(() =>{
-    return this.carrinho().reduce((total,item) =>
-      total + item.preco,0)});
+
+  // ======== MÉTODO SET ===============
+  substituirProdutos(){
+    this.produtos.set([
+      { nome:'Teclado', preco: 50 },
+      { nome:'Mouse', preco: 15 },
+      { nome:'Monitor', preco: 500 },
+      { nome:'Desktop', preco: 1500 },
+      { nome:'Headset', preco: 30 },
+    ]);
+  }
+
+  exibirProduto(nome: string){
+    console.log('Produto Selecionado: ', nome);
+    this.produtoSelecionado.set(nome);
+  }  
 }
